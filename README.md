cc4e505092e80cc51a3ef23383ded1c0c54393c1
lohikrme <95482953+lohikrme@users.noreply.github.com>
2023-01-29 19:57:48 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,42 @@
+# Roll20-Dice-Roller-1.0.1
+To automatize rolling dice in roll20.net.
+
+
+Hello! This is my Roll20 Dice Roller version 1.0.1. 
+
+I am lohikrme from github, here is my account: https://github.com/lohikrme
+
+All rights reserved. For personal use only.
+
+
+Last time updated: 1/5/2023.
+
+
+
+How to use it?:
+
+So, this program requires user to copy the javascript code, 
+open F12 browser inspector, and there create a snippet. Paste all content into the snipper, 
+and give it a descriptive name, and end the name with the tag ".js". 
+After that open the roll20.net website, there gaming area, and after that 
+simply press the "run" button for the snipper. The snippet will therefore be saved inside your browser, 
+you do not need to create the snipper but once. But always when u reopen roll20.net you need to again activate it 
+by pressing F12 and there button "run". This is because browser automatically turns off all snippets after session.
+
+
+What does it do?:
+
+This snippet replaces the "move mouse to right down and click the chatting area 
+-> write /roll 2d6 -> press enter -> move mouse back to map" process, 
+and allows user to simply press a number key between 1 and 9, and the pc will do all that for you! 
+
+
+Is it safe?:
+
+There is only about 100 lines of code. The snippet does not modify the website anyhow. 
+Everyone can see what the code does. It does not share data to anywhere or even collect data. 
+It simply creates a macro for keyboard keys that works only on that specific website by reading the 
+website html code's classes and ids. There is maybe 0.000000001% chance that some other website would 
+have a chatting area with exact same class and id, but in that case the worst what could happen would be 
+to accidentically write in chat "/roll 4d6". The snippet can be easily deactivated or deleted, 
+and it will also deactivate itself everytime the browser is turned off from X.
---
+++ roll20_dice_roller_1.0.1.js
@@ -0,0 +1,153 @@
+
+/* ----------Roll20 Dice Roller 1.0.1---------- */
+/* Hello! I hope  you enjoy this script I wrote for Roll20.net to automatize rolling dices.
+So, whenever an user presses a number between 1-9 in his keyboard, this script will automatically do next:
+first left-clicks the chat in roll20 gaming website, then writes "/roll xd6" and then presses enter. 
+Imagine rolling the dice 300 times in a single game - this script will save u about 300 clicks and 2700 keys.
+
+I wrote this script 25.12.2022 - 5.1.2023. For personal use only. All rights reserved. 
+
+Link to my GitHub: https://github.com/lohikrme */
+
+
+
+
+
+/* ----------The Structure of This Code---------- */
+/* First this code creates variables using roll20.net chatbox' ID and class, which allows making inputs with code. 
+Second, the code also creates an EventListener, which reads user keyboard, and sends forward to KeyUpHandler function
+information about pressed keys (the information is in form of keyCodes). KeyUpHandler function compares pressed keys to
+keyCode values 49 to 57, and if there is a match, it activates a suitable Rollxd6 function. (Note that in keyCodes 1 = 49, 2 = 50, etc).
+Rollxd6 functions will write the input (such as /roll 2d6) into the chatbox and then click the send-button. */
+
+
+
+/* ----------Basic Variables and the EventListener---------- */
+/* querySelector creates a variable of html classes and identities. var textarea and var send_button are 
+the html elements found in the chatbox - the writing area and the send-button. 
+There is also an EventListener, which sends information about keyboard activity for KeyUpHandler function. 
+Note that it is "keyup", which means that it works only on release of a key. */
+
+var textarea = document.querySelector("#textchat-input .ui-autocomplete-input")
+var send_button = document.querySelector("#textchat-input .btn")
+window.addEventListener("keyup", KeyUpHandler, false)
+
+
+
+/* ----------KeyUpHandler function---------- */
+/* KeyUpHandler function reads the event from window.addEventListener method. This EventListener allows this function to know 
+when an user has pressed a key between 1 and 9, by comparing received keyCodes. All keyboards have the same keyCodes, 
+so when an user presses 1, he actually presses keyCode 49, and so on, until keyCode 57 / key 9. 
+So, this function receives information from the event from EventListener, and then when keyCode is between 49 and 57,
+this function will activate a suitable Rollxd6 function matching with the pressed key. */
+
+function KeyUpHandler (event) {
+
+    console.log(event)
+
+    /* User presses key 1 */
+    if (event.keyCode === 49) {
+        Roll1D6()
+    }
+
+    /* User presses key 2 */
+    else if (event.keyCode === 50) {
+        Roll2D6()
+    }
+
+    /* User presses key 3 */
+    else if (event.keyCode === 51) {
+        Roll3D6()
+    }
+
+    /* User presses key 4 */
+    else if (event.keyCode === 52) {
+        Roll4D6()
+    }
+
+    /* User presses key 5 */
+    else if (event.keyCode === 53) {
+        Roll5D6()
+    }
+
+    /* User presses key 6 */
+    else if (event.keyCode === 54) {
+        Roll6D6()
+    }
+
+    /* User presses key 7 */
+    else if (event.keyCode === 55) {
+        Roll7D6()
+    }
+
+    /* User presses key 8 */
+    else if (event.keyCode === 56) {
+        Roll8D6()
+    }
+
+    /* User presses key 9 */
+    else if (event.keyCode === 57) {
+        Roll9D6()
+    }
+
+}
+
+
+
+/* ----------RollxD6 functions---------- */
+/* These functions are activated if an user presses a keyboard number between 1 and 9. After activation, this function
+will first click the textarea, then write "/roll xd6", and then clicks the sending-button. */
+
+/* roll 1d6 */
+function Roll1D6() {
+    textarea.value = "/roll 1d6"
+    send_button.click()
+}
+
+/* roll 2d6 */
+function Roll2D6() {
+    textarea.value = "/roll 2d6"
+    send_button.click()
+}
+
+/* roll 3d6 */
+function Roll3D6() {
+    textarea.value = "/roll 3d6"
+    send_button.click()
+}
+
+/* roll 4d6 */
+function Roll4D6() {
+    textarea.value = "/roll 4d6"
+    send_button.click()
+}
+
+/* roll 5d6 */
+function Roll5D6() {
+    textarea.value = "/roll 5d6"
+    send_button.click()
+}
+
+/* roll 6d6 */
+function Roll6D6() {
+    textarea.value = "/roll 6d6"
+    send_button.click()
+}
+
+/* roll 7d6 */
+function Roll7D6() {
+    textarea.value = "/roll 7d6"
+    send_button.click()
+}
+
+/* roll 8d6 */
+function Roll8D6() {
+    textarea.value = "/roll 8d6"
+    send_button.click()
+}
+
+/* roll 9d6 */
+function Roll9D6() {
+    textarea.value = "/roll 9d6"
+    send_button.click()
+}
